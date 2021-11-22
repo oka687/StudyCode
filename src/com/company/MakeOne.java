85601b95b193d1e361c2cbdbadfe2072b3d02eb2
oka687 <oka684@naver.com>
2021-11-22 23:31:06 +0900

    1로 만들기(코드재수정)

--- src/com/company/MakeOne.java
+++ src/com/company/MakeOne.java
@@ -4,69 +4,26 @@ import java.util.Scanner;
 
 public class MakeOne {
 
-    // 선언부
-    int firstNum;
-    String getNum;
-    int calcNum;
-    int timeCount = 0;
-
     public void divid(){
         Scanner sc = new Scanner(System.in);
 
-        // 입력부
         System.out.println("숫자 입력");
-        calcNum = sc.nextInt();
-
+        int N=sc.nextInt();
+        int[] dp=new int[N+1];
         sc.close();
-        // 계산부
-        while(true) {
-            calcNum = resultQuest(calcNum);
-            timeCount++;
-            if(calcNum == 1){
-                break;
+        for(int i=2;i<=N;i++) {
+            dp[i]=dp[i-1]+1;
+            if(i%2==0) {
+                dp[i]=Math.min(dp[i], dp[i/2]+1);
+            }
+            if(i%3==0) {
+                dp[i]=Math.min(dp[i], dp[i/3]+1);
+            }
+            if(i%5==0) {
+                dp[i]=Math.min(dp[i], dp[i/5]+1);
             }
         }
-        // 횟수 카운트
-        System.out.println(timeCount);
-    }
-
-    public int resultQuest(int calcNum) {
-
-        // 끝자리를 얻기 위해 String으로 변수
-        getNum = String.valueOf(calcNum);
-
-        // 자리수가 1을 넘기면 끝자리만 떼오기
-        if (getNum.length() > 1) {
-            firstNum = Integer.parseInt(getNum.substring(getNum.length() - 1));
-        } else if (calcNum != 1) {
-            // 자리수가 1자리이고 1이 아니라면 그대로 가져오기
-            firstNum = calcNum;
-        }
-
-        // 비교 후 계산
-        switch (firstNum) {
-            case 0:
-                return calcNum / 5;
-            case 1:
-                return calcNum - 1;
-            case 2:
-                return calcNum / 2;
-            case 3:
-                return calcNum / 3;
-            case 4:
-                return calcNum / 2;
-            case 5:
-                return calcNum / 5;
-            case 6:
-                return calcNum - 1;
-            case 7:
-                return calcNum - 1;
-            case 8:
-                return calcNum / 2;
-            case 9:
-                return calcNum / 3;
-
-            default: return 0;
-        }
+        System.out.println(dp[N]);
     }
 }
+

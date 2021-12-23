401d78cb61e61bd15dddf264470720e21cb56dc2
oka687 <oka684@naver.com>
2021-12-23 20:34:50 +0900

    곱하기혹은더하기_설명추가

--- src/com/company/AddOrMulti.java
+++ src/com/company/AddOrMulti.java
@@ -14,13 +14,18 @@ public class AddOrMulti {
         str = sc.nextLine();
         array = new int[str.length()];
 
+        // 문자열을 숫자로 바꿔서 배열에 담기
         for(int i = 0; i < str.length(); i++){
             array[i] = Integer.parseInt(String.valueOf(str.charAt(i)));
         }
 
+        //정렬
         Arrays.sort(array);
 
+        // 첫번째 값 대입
         count = array[0];
+
+        // 두 수를 계산해서 값이 더 큰 쪽으로 계산하기
         for (int l = 1; l < array.length; l++) {
             if(count + array[l] > count * array[l]){
                 count = count + array[l];

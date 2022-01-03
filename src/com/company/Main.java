965f7abb63e5926eb05b4c89eef38c6800fd2942
oka687 <oka684@naver.com>
2022-01-03 21:20:23 +0900

    문자열 재정렬

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,7 +8,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        LuckyStr str = new LuckyStr();
-        str.skill();
+        ReString str = new ReString();
+
+        str.rebuild();
+
     }
 }
---
+++ src/com/company/ReString.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class ReString {
+
+    public void rebuild(){
+
+       // String array = "K1KA5CB7";
+        String array = "AJKDLSI412K4JSDJ9D";
+
+        int arrayAdd = 0;
+
+        String strNum = array.replaceAll("[a-zA-Z]","");    // 숫자만
+        String str = array.replaceAll("[0-9]","");          // 문자열만
+
+        // 숫자 합
+        for(int i = 0; i < strNum.length(); i++){
+            int bucket = Character.getNumericValue(strNum.charAt(i));
+            arrayAdd = arrayAdd + bucket;
+        }
+
+        // CHAR 배열로 된 문자열 값을 String으로 변환
+        char[] arrStr = str.toCharArray();
+        Arrays.sort(arrStr);
+
+        // 각 값 합치기
+        String answer = new String(arrStr);
+        String arrayNum = Integer.toString(arrayAdd);
+
+        System.out.println(answer + arrayNum);
+    }
+}

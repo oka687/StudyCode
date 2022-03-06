6c84079a250b851951ae9d7a34c23b435aedb031
oka687 <oka684@naver.com>
2022-03-06 15:37:51 +0900

    안테나(주석추가)

--- src/com/company/Andena.java
+++ src/com/company/Andena.java
@@ -6,11 +6,10 @@ public class Andena {
 
     public void link(){
 
+        // 조건 선언
         Scanner sc = new Scanner(System.in);
-
         HashMap<Integer,Integer> ary = new HashMap<>();
         int N = sc.nextInt();
-
         int [] arr = new int[N];
 
         // 난수생성
@@ -23,25 +22,25 @@ public class Andena {
         // 줄바꿈
         System.out.println("");
 
-
+        // 거리를 계산한 뒤 결과값을 해쉬맵에 저장한다
         for(int l = 0; l < arr.length; l++){
             int mo = 0;
             for(int t = 0; t < arr.length; t++){
                 int a = Math.abs(arr[l] - arr[t]);
                 mo = mo + a;
             }
-
             ary.put(arr[l],mo);
         }
 
+        // 키값을 저장하기 위한 리스트 선언
         List<Integer> key = new ArrayList<>(ary.keySet());
-
+        
+        // 정렬
         Collections.sort(key, (o1, o2) -> (ary.get(o1).compareTo(ary.get(o2))));
 
-   /*     for(Integer key1 : key){
-            System.out.println(key1+"/"+ary.get(key1));
-        }*/
         // 키값 출력
         System.out.println(key.get(0));
+
+        sc.close();
     }
 }

be57549878123d361e2b63f947bf82b823b32594
oka687 <oka684@naver.com>
2022-03-07 21:50:39 +0900

    실패율(프로그래머스에서 품)재업

--- src/com/company/failMath.java
+++ src/com/company/failMath.java
@@ -49,3 +49,44 @@ public class failMath {
 
     }
 }
+
+
+//프로그래머스 코드
+//import java.util.*;
+//        import java.util.List;
+//
+//class Solution {
+//    public int[] solution(int N, int[] stages) {
+//        int[] answer = new int[N];
+//        HashMap<Integer,Double> ary = new HashMap<>();
+//
+//        double human = stages.length;
+//        N = N + 1;
+//
+//        for(int i = 1; i < N; i++){
+//            int count = 0;
+//            for(int l = 0; l < stages.length; l++){
+//                if(i == stages[l]){
+//                    count++;
+//                }
+//            }
+//            if(count == 0){
+//                ary.put(i,0.0);
+//            }else{
+//                human = human - count;
+//
+//                double a = count / human;
+//                ary.put(i,a);
+//            }
+//        }
+//
+//        List<Integer> key = new ArrayList<>(ary.keySet());
+//        Collections.sort(key, (o1, o2) -> (ary.get(o2).compareTo(ary.get(o1))));
+//
+//        for(int a = 0; a < key.size(); a++){
+//            answer[a] = key.get(a);
+//        }
+//
+//        return answer;
+//    }
+//}
